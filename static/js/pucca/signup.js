242bde9a1a16725fc23497a6654c77b1b5e89116
Valeria Marquez <valeriamarquezvelazques14@gmail.com>
2026-03-25 10:10:11 -0700

    primer commit

---
+++ static/js/pucca/signup.js
@@ -0,0 +1,60 @@
+   
+document.getElementById('btn-register').addEventListener("click",register);
+
+    function register(){
+         const password=document.getElementById("user-password").value;
+         const password2=document.getElementById("user-repeat-password").value;
+
+        if (password!=password2){
+           Swal.fire({
+            icon: 'warning',
+            title: 'Contraseñas no coinciden',
+            text: 'Por favor verifica que ambas contraseñas sean iguales',
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'Entendido',
+        });
+            return;
+            //Sweetalert
+        }
+
+        const data= {
+            name: document.getElementById('user-name').value,
+            email:document.getElementById("user-email").value,
+            password:document.getElementById("user-password").value
+        }
+
+        fetch('api/users', {
+            method: 'POST', 
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(data)
+        }).then(response=> response.json())
+        .then(result=>{
+            if(result.success){
+                Swal.fire({
+                    icon: 'success',
+                    title: '¡Registro exitoso!',
+                    text: 'Usuario guardado correctamente',
+                    confirmButtonColor: '#3085d6',
+                    confirmButtonText: 'Continuar'})
+            }else{
+                 Swal.fire({
+                icon: 'error',
+                title: 'Error al registrar',
+                text: result.message || 'Ocurrió un error al guardar el usuario',
+                confirmButtonColor: '#d33',
+                confirmButtonText: 'Intentar de nuevo'
+            });
+            }
+
+        })
+        .catch(error=>{
+            console.error('Error:', error);
+            Swal.fire({
+            icon: 'error',
+            title: 'Error de conexión',
+            text: 'No se pudo conectar con el servidor',
+            confirmButtonColor: '#d33',
+            confirmButtonText: 'OK'
+            });
+        });
+}

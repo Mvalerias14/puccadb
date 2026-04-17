c83ec2f10ed05a0bdd39982b30c8a0573be604cd
Valeria Marquez <valeriamarquezvelazques14@gmail.com>
2026-04-17 09:13:32 -0700

    ultimos cambio

--- entities/account.py
+++ entities/account.py
@@ -14,43 +14,8 @@ class Account():
         self.user = user
         self.transactions = transactions
 
-    # 🔹 Obtener cuenta por ID de cuenta
-    @staticmethod
-    def get_by_id(id_account: int):
-        try:
-            connection = get_connection()   
-            cursor = connection.cursor(pymysql.cursors.DictCursor)    
 
-            sql = "SELECT id, number, creation_date, user_id FROM account WHERE id = %s"
-            cursor.execute(sql, (id_account,))
 
-            rs = cursor.fetchone()
-
-            if rs:
-                user = User.get_by_id(rs["user_id"])
-                transactions = Transaction.get_transactions_by_account(rs["id"])
-
-                account = Account(
-                    rs["id"],
-                    rs["number"],
-                    rs["creation_date"],
-                    user,
-                    transactions
-                )
-
-                return account
-
-            return None
-
-        except Exception as ex:
-            print(f"Error getting account: {ex}")
-            return None
-
-        finally:
-            cursor.close()
-            connection.close()
-
-    # 🔹 Obtener cuenta por ID del usuario (ESTE ES EL QUE NECESITAS)
     @staticmethod
     def get_account_by_id(user_id: int):
         try:
